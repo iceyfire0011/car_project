999207f0888cb1bc02ab87375083da8aa1605a07
DESKTOP-FA5RKVS\Bismillah pc <s.m.abdulwassae@gnail.com>
2021-06-14 22:06:34 +0600

    Transfer to dev

---
+++ src/com/ExceptionDemo/Car.java
@@ -0,0 +1,13 @@
+package com.ExceptionDemo;
+
+public class Car {
+    public static String color = "Red";
+
+    int price(int amount){
+        return amount;
+    }
+
+    String color(){
+        return color;
+    }
+}
--- src/com/ExceptionDemo/Main.java
+++ src/com/ExceptionDemo/Main.java
@@ -3,6 +3,9 @@ package com.ExceptionDemo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Car car = new Car();
+        System.out.println("price= " + car.price(500000));
+        System.out.println("color= " + car.color());
     }
 }
